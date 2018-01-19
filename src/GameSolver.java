d643f868bd072d236e0783f06ef568f0a237fc97
Kanchanok Kannee <kanchanok.kan@ku.th>
2018-01-19 13:29:32 +0700

    Fixed javadoc & GameSolver

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -17,27 +17,21 @@ public class GameSolver {
 	 */
 	public int play(NumberGame game) {
 
-		System.out.println(game.toString());
 		int takeupperBound = game.getUpperBound() / 4;
 		int guess = game.getUpperBound() / 2;
 		while (!game.guess(guess)) {
 
-			System.out.print("Your answer? ");
 			if (game.getMessage().contains("too small")) {
-				System.out.println(guess);
 				guess = guess + takeupperBound;
 			} else if (game.getMessage().contains("too large")) {
-				System.out.println(guess);
 				guess = guess - takeupperBound;
 			}
 			if (takeupperBound != 1) {
 				takeupperBound = takeupperBound / 2;
 			}
 
-			System.out.println(game.getMessage());
-
 		}
-		System.out.println("Right! The secret number is " + guess);
+
 		return guess;
 
 	}
